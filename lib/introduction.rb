ee0d6443d64a0b900c3de3325f76466973ef0f2e
Edward Lindsey <eddielindsey23@gmail.com>
2020-02-03 05:14:18 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,11 @@
-def Josh 
-  "Hi, my name is Josh."
+def introduction
 end
-def Dan, "Ember.js"
-  "Hi, my name is Dan and I am learning to program in Ember.js."
+def introduction(name)
 end
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
